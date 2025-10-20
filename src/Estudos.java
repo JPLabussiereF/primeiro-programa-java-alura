248d23566bf120e6c207e296cbeca7f201c07c3b
Joao Labussiere <labussework@gmail.com>
2025-10-20 19:57:52 -0300

    01 - Alura

---
+++ src/Estudos.java
@@ -0,0 +1,8 @@
+public class Estudos {
+    public static void main(String[] args){
+        System.out.println("Minhas anotações de Java:");
+        System.out.println("1. Uma classe é um 'molde' para criar objetos.");
+        System.out.println("2. O método 'main' é onde o programa começa a ser executado.");
+        System.out.println("3. 'System.out.println()' imprime uma linha no console.");
+    }
+}
---
+++ src/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil {
+    public static void main(String[] args){
+        System.out.println("Olá" + " João Pedro");
+
+        System.out.println("Tudo bem?");
+    }
+}
